40de3d4475235ee187c1303463b3e9af0f163aca
Bolaji <holyboboli@pop-os.localdomain>
2021-01-27 00:38:33 -0500

    Just getting started

---
+++ pandaservice/src/main/java/com/holyboboli/pandaservice/App.java
@@ -0,0 +1,13 @@
+package com.holyboboli.pandaservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
